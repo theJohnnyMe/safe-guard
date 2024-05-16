083f15434d8a8bda97ebe6cdc53332091e15639f
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2024-05-16 13:56:34 +0200

    fix: styling of landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,19 +6,34 @@ export default function Home() {
   return (
     <section>
       <div className="container">
-        <h1 className="tds-headline-01 tds-u-p0 tds-u-m0">Scania</h1>
-        <h1 className="tds-headline-01 tds-u-p0 tds-u-m0">SafeGuard</h1>
-        <p className="tds-paragraph-02">
-          Personalised insurance based on your driving and maintenance.
-        </p>
-
-        <div className="license-plate">
-          <div className="eu-section">
-            <div className="eu-stars"></div>S
-          </div>
-          <input type="text" maxLength={6} placeholder="ABC 123" />
+        <div className="start-top">
+          <h1 className="tds-headline-01 tds-u-p0 tds-u-m0">Scania</h1>
+          <h1 className="tds-headline-01 tds-u-p0 tds-u-m0">SafeGuard</h1>
+          <p className="tds-paragraph-02">
+            Personalised insurance based on your driving and maintenance.
+          </p>
         </div>
-        <TdsButton text="See your price" fullbleed></TdsButton>
+
+        <div className="start-center">
+          <p className="tds-detail-02 tds-u-opacity-60 tds-u-pb1 tds-u-m0">
+            Enter registration number
+          </p>
+
+          <div className="license-plate tds-u-mb2">
+            <div className="eu-section">
+              <div className="eu-stars"></div>S
+            </div>
+            <input type="text" maxLength={6} placeholder="ABC 123" />
+          </div>
+          <TdsButton text="See your price" fullbleed></TdsButton>
+        </div>
+
+        <div className="start-bottom">
+          <p className="tds-detail-02 tds-u-opacity-60 tds-u-pb1 tds-u-m0">
+            Already a customer?
+          </p>
+          <TdsButton text="Log in" fullbleed variant="secondary"></TdsButton>
+        </div>
       </div>
     </section>
   );
