deda496f21c8aa85463052e8113cf5e08c93dce9
theJohnnyMe <nikola.johnny.mirkovic@scania.com>
2024-05-16 15:28:17 +0200

    feat: loading page work

---
+++ src/app/components/progress-bar/progress-bar.tsx
@@ -0,0 +1,15 @@
+"use client";
+import "./progress-bar.scss";
+
+export default function ProgressBar({ width = 0 }) {
+  return (
+    <div className="w3-container">
+      <div className="w3-light-grey w3-round-xlarge">
+        <div
+          className="w3-container w3-blue w3-round-xlarge"
+          style={{ height: "5px", width: `${width}%` }}
+        ></div>
+      </div>
+    </div>
+  );
+}
