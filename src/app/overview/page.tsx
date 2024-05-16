98a1e63dc04230038cdadead509c3fbdc6e58f64
Marcus Mena Pacheco <marcus.mena.pacheco@scania.com>
2024-05-16 11:29:26 +0200

    add overview route

---
+++ src/app/overview/page.tsx
@@ -0,0 +1,16 @@
+import "./page.scss";
+
+export default function Overview() {
+  return (
+    <main>
+      <div className="overview">
+        <div className="overview__price">
+          <p>Your price</p>
+          <h1>
+            1100 <span>kr/mo</span>
+          </h1>
+        </div>
+      </div>
+    </main>
+  );
+}
